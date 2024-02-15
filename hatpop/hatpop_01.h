951117b6610470adbf466d0234bf3e36f417f79e
phucvin <phucvin@gmail.com>
2024-02-15 19:26:41 +0000

    cont

--- hatpop/hatpop_01.h
+++ hatpop/hatpop_01.h
@@ -1,3 +1,5 @@
+// Simple pointer handle store (i.e. the handle type is a raw pointer)
 #include "pointer_handle_store.h"
 #include "common.h"
+// Raw delete (i.e. `delete ptr`) when `Owned` releases
 #include "raw_delete_owned.h"
