60a066646530f78fe74adf3f234b271d095fdd5b
phucvin <phucvin@gmail.com>
2024-02-15 23:49:58 +0000

    shared_ptr for shared mutex to ensure the mutex is not deleted while still being used

--- hatpop/examples/simple01.cpp
+++ hatpop/examples/simple01.cpp
@@ -1,7 +1,7 @@
 #include <iostream>
 #include <string>
 
-#include "../hatpop_05.h"
+#include "../hatpop_06.h"
 
 class UserService {
 private:
--- hatpop/owned_and_temp_ptr/shared_mutex.h
+++ hatpop/owned_and_temp_ptr/shared_mutex.h
@@ -1,6 +1,7 @@
 #pragma once
 
 #include <shared_mutex>
+#include <memory>
 
 namespace htp {
 
@@ -8,17 +9,21 @@ template<typename T>
 class TempPtr {
 private:
     T* ptr_;
-    std::shared_mutex* mu_;
+    std::shared_ptr<std::shared_mutex> mu_;
 
 public:
-    TempPtr(Handle handle, std::shared_mutex* mu) : ptr_(nullptr), mu_(mu) {
+    TempPtr(Handle handle, std::shared_ptr<std::shared_mutex> mu)
+            : ptr_(nullptr), mu_(std::move(mu)) {
         void* tmp1 = HandleStore::GetSingleton()->GetUnsafe(handle);
-        if (!tmp1) return;
-        // TODO: Make sure mu_ is not yet deleted here
+        if (!tmp1) {
+            mu_.reset();
+            return;
+        }
         mu_->lock_shared();
         void* tmp2 = HandleStore::GetSingleton()->GetUnsafe(handle);
         if (tmp2 != tmp1) {
             mu_->unlock_shared();
+            mu_.reset();
             return;
         }
         ptr_ = (T*)tmp2;
@@ -46,10 +51,11 @@ template<typename T>
 class Unowned {
 private:
     Handle handle_;
-    std::shared_mutex* mu_;
+    std::shared_ptr<std::shared_mutex> mu_;
 
 public:
-    Unowned(Handle handle, std::shared_mutex* mu) : handle_(handle), mu_(mu) {}
+    Unowned(Handle handle, std::shared_ptr<std::shared_mutex> mu)
+            : handle_(handle), mu_(std::move(mu)) {}
 
     TempPtr<T> GetTempPtr() const {
         return TempPtr<T>(handle_, mu_);
@@ -61,11 +67,12 @@ class Owned {
 private:
     T* ptr_;
     Handle handle_;
-    std::shared_mutex mu_;
+    std::shared_ptr<std::shared_mutex> mu_;
 
 public:
-    Owned(T* ptr) : ptr_(ptr) {
+    Owned(T* ptr) : ptr_(ptr), mu_(std::make_shared<std::shared_mutex>()) {
         if (ptr_) handle_ = HandleStore::GetSingleton()->Create(ptr_);
+        else mu_.reset();
     }
 
     // This type is moveable but not copyable
@@ -82,11 +89,11 @@ public:
     }
 
     Unowned<T> GetUnowned() {
-        return Unowned<T>(handle_, &mu_);
+        return Unowned<T>(handle_, mu_);
     }
 
     TempPtr<T> GetTempPtr() {
-        return TempPtr<T>(handle_, &mu_);
+        return TempPtr<T>(handle_, mu_);
     }
 
     void Release() {
@@ -96,8 +103,8 @@ public:
         auto tmp = ptr_;
         ptr_ = nullptr;
         handle_ = {};
-        mu_.lock();
-        mu_.unlock();
+        mu_->lock();
+        mu_->unlock();
         delete tmp;
     }
 };
