358f14b8dd4976cfc53183af1a707ae9ce6cfff2
phucvin <phucvin@gmail.com>
2024-02-16 23:39:05 +0000

    skeleton benchmark time01

--- hatpop/benchmarks/time01.cpp
+++ hatpop/benchmarks/time01.cpp
@@ -1,7 +1,10 @@
+#include "../hatpop01.h"  // ?
+// #include "../hatpop04.h"  // ?
+
 #include "../third_party/ubench.h"
 
-UBENCH(foo, bar) {
-  usleep(100 * 1000);
+UBENCH(Time01, make_owned) {
+    auto x = hatp::make_owned<int>(1);
 }
 
 UBENCH_MAIN();
