045d50d71de856b6e5c7d1416d9b13486f94272b
phucvin <phucvin@gmail.com>
2024-02-17 06:25:01 +0000

    rc01

--- hatpop/hatpop12.h
+++ hatpop/hatpop12.h
@@ -3,9 +3,5 @@
 // Simple pointer handle store (i.e. the handle type is a raw pointer)
 #include "handle_store/raw_pointer.h"
 #include "common_no_unowned.h"
-// Atomic Reference Counter 
-#include "owned_and_temp_ptr/arc02.h"
-// NOTE: Others don't work with raw_pointer, i.e. will get use-after-free errors,
-// since they need to call HandleStore::GetUnsafe before doing further bookeeping
-// ARC works in *most cases* (not yet tested for all possible cases) because it
-// first checks the atomic ref count > 0 before calling HandleStore:GetUnsafe
+// Non-atomic Reference Counter 
+#include "owned_and_temp_ptr/rc01.h"
---
+++ hatpop/owned_and_temp_ptr/rc01.h
@@ -0,0 +1,117 @@
+#pragma once
+
+#include <atomic>
+#include <memory>
+#include <cassert>
+
+namespace hatp {
+
+template<typename T>
+class TempPtr {
+private:
+    T* ptr_;
+    int* rc_;
+
+public:
+    TempPtr(Handle handle, int* rc) : ptr_(nullptr), rc_(rc) {
+        if (*rc_ == 0) {
+            rc_ = nullptr;
+            return;
+        }
+        ptr_ = (T*)HandleStore::GetSingleton()->GetUnsafe(handle);
+        if (ptr_ == nullptr) {
+            rc_ = nullptr;
+            return;
+        }
+        *rc_ += 1;
+    }
+
+    // This type is neither moveable nor copyable
+    TempPtr(TempPtr&& rhs) = delete;
+    TempPtr(const TempPtr&) = delete;
+    ~TempPtr() { Release(); }
+    T operator *() const { return *Get(); }
+    T* operator ->() const { return Get(); }
+    operator bool() const { return Get() != nullptr; }
+
+    T* Get() const { return ptr_; }
+
+    void Release() {
+        if (ptr_ == nullptr) return;
+
+        if (--*rc_ == 0) {
+            auto tmp = ptr_;
+            ptr_ = nullptr;
+            delete tmp;
+            delete rc_;
+            rc_ = nullptr;
+        }
+    }
+};
+
+template<typename T>
+class Unowned {
+private:
+    Handle handle_;
+    int* rc_;
+
+public:
+    Unowned(Handle handle, int* rc) : handle_(handle), rc_(rc) {}
+
+    TempPtr<T> GetTempPtr() const {
+        return TempPtr<T>(handle_, rc_);
+    }
+};
+
+template<typename T>
+class Owned {
+private:
+    T* ptr_;
+    Handle handle_;
+    int* rc_;
+
+public:
+    Owned(T* ptr) {
+        if (ptr) {
+            ptr_ = ptr;
+            rc_ = new int(1);
+            handle_ = HandleStore::GetSingleton()->Create(ptr_);
+        }
+    }
+
+    // This type is moveable but not copyable
+    Owned(Owned&& rhs) {
+        this->ptr_ = rhs.ptr_;
+        this->handle_ = rhs.handle_;
+        rhs.ptr_ = nullptr;
+        rhs.handle_ = {};
+    }
+    // Common methods & operators
+    Owned(const Owned&) = delete;
+    ~Owned() { Release(); }
+    operator Unowned<T>() { return GetUnowned(); }
+
+    Unowned<T> GetUnowned() const {
+        return Unowned<T>(handle_, rc_);
+    }
+
+    TempPtr<T> GetTempPtr() const {
+        return TempPtr<T>(handle_, rc_);
+    }
+
+    void Release() {
+        if (ptr_ == nullptr) return;
+        
+        HandleStore::GetSingleton()->Erase(handle_);
+        auto tmp = ptr_;
+        ptr_ = nullptr;
+        handle_ = {};
+        if (--*rc_ == 0) {
+            delete rc_;
+            delete tmp;
+        }
+        rc_ = nullptr;
+    }
+};
+
+}  // namespace hatp
