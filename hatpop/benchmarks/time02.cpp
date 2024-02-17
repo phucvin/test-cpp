78610f01fa49a45b61b75cc49b30bc5c0184ec67
phucvin <phucvin@gmail.com>
2024-02-17 00:01:19 +0000

    bench ReadRelease2Threads

--- hatpop/benchmarks/time02.cpp
+++ hatpop/benchmarks/time02.cpp
@@ -1,11 +1,31 @@
-// #include "../hatpop01.h"
-// #include "../hatpop04.h"
-#include "../hatpop07.h"
+#include <atomic>
 
+#include "../hatpop01.h"
+// #include "../hatpop04.h"
+// #include "../hatpop07.h"
+
+#include "../third_party/thread_pool.h"
 #include "../third_party/ubench.h"
 
+ctpl::thread_pool _thread_pool(2);
+
 UBENCH(Time02, ReadRelease2Threads) {
-    auto x = hatp::make_owned<int>(1);
+    auto owned_x = hatp::make_owned<int>(1);
+    auto unowned_x = owned_x.GetUnowned();
+    std::atomic_int done_count;
+
+    _thread_pool.push([&](int) {
+        if (auto x = unowned_x.GetTempPtr()) {
+            assert(*x == 1);
+        }
+        done_count.fetch_add(1);
+    });
+    _thread_pool.push([&](int) {
+        owned_x.Release();
+        done_count.fetch_add(1);
+    });
+
+    while (done_count.load() < 2) continue;
 }
 
 UBENCH_MAIN();
--- hatpop/examples/race02.cpp
+++ hatpop/examples/race02.cpp
@@ -21,7 +21,7 @@ int main() {
     hatp::Owned<int> owned_foo1 = hatp::make_owned<int>(1);
     hatp::Owned<int> owned_foo2 = hatp::make_owned<int>(2);
     std::jthread t1([&owned_foo1] {
-        if (auto foo1 = owned_foo1.GetTempPtr(); foo1) {
+        if (auto foo1 = owned_foo1.GetTempPtr()) {
             std::this_thread::sleep_for(std::chrono::seconds(1));
             std::cout << "Done keeping foo1!" << std::endl;
         }
