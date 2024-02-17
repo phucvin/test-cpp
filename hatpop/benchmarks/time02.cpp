6377401e1c6b3e852331ff7a17a59ef36fcde57a
phucvin <phucvin@gmail.com>
2024-02-17 00:27:02 +0000

    Read100

--- hatpop/benchmarks/time02.cpp
+++ hatpop/benchmarks/time02.cpp
@@ -1,23 +1,39 @@
 #include <atomic>
+#include <iostream>
 #include <cassert>
 
-// #include "../hatpop01.h"  // Error
+#include "../hatpop01.h"  // Error
 // #include "../hatpop04.h"
-#include "../hatpop07.h"
+// #include "../hatpop05.h"
+// #include "../hatpop07.h"
 
 #include "../third_party/thread_pool.h"
 #include "../third_party/ubench.h"
 
-ctpl::thread_pool _thread_pool(2);
+constexpr int _max_threads = 101;
+ctpl::thread_pool _thread_pool(_max_threads + 2);
 
-UBENCH(Time02, ReadRelease2Threads) {
+UBENCH(Time02, WarmUp) {
+    std::atomic_int done_count;
+    for (int i = 0; i < _max_threads; ++i) {
+        _thread_pool.push([&](int) {
+            done_count.fetch_add(1);
+        });
+    }
+    while (done_count.load() < _max_threads) continue;
+}
+
+UBENCH(Time02, Read1Release1) {
     auto owned_x = hatp::make_owned<int>(1);
     auto unowned_x = owned_x.GetUnowned();
     std::atomic_int done_count;
+    static std::atomic_int _printed_err = 0;
 
     _thread_pool.push([&](int) {
         if (auto x = unowned_x.GetTempPtr()) {
-            assert(*x == 1);
+            if (*x != 1 && _printed_err.fetch_add(1) == 0) {
+                std::cerr << "ERROR: *x != 1" << std::endl;
+            }
         }
         done_count.fetch_add(1);
     });
@@ -27,6 +43,23 @@ UBENCH(Time02, ReadRelease2Threads) {
     });
 
     while (done_count.load() < 2) continue;
+}
+
+UBENCH(Time02, Read100) {
+    auto owned_x = hatp::make_owned<int>(1);
+    auto unowned_x = owned_x.GetUnowned();
+    std::atomic_int done_count;
+
+    for (int i = 0; i < _max_threads; ++i) {
+        _thread_pool.push([&](int) {
+            auto x = unowned_x.GetTempPtr();
+            assert(x);
+            assert(*x == 1);
+            done_count.fetch_add(1);
+        });
+    }
+
+    while (done_count.load() < _max_threads) continue;
 }
 
 UBENCH_MAIN();
