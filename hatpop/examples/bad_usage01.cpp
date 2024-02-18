fc84f5cb69df382516541b3d25a778bf0851ad5a
phucvin <phucvin@gmail.com>
2024-02-18 17:36:33 +0000

    use_temp_ptr_in_another_thread

--- hatpop/examples/bad_usage01.cpp
+++ hatpop/examples/bad_usage01.cpp
@@ -1,7 +1,28 @@
 // TODOs:
-// - Use temp ptr in another thread
 // - Copy / move temp ptr
 
-void bad_use_temp_ptr_in_another_thread() {
+#include <future>
 
+#include "../hatpop07.h"
+
+// Might randomly lead to use-after-free
+void use_temp_ptr_in_another_thread() {
+    auto x = hatp::make_owned<int>(1);
+    if (auto tp = x.GetTempPtr()) {
+        auto unused = std::async(std::launch::async, [&] {
+            assert(*tp == 1);
+        });
+    }
+    // This is similar to a bad practice of keeping reference/pointer to a local
+    {
+        int y = 2;
+        auto unused = std::async(std::launch::async, [&] {
+            assert(y == 2);
+        });
+    }
+}
+
+int main() {
+    use_temp_ptr_in_another_thread();
+    return 0;
 }
